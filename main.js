3a73197ab178de2aae31ff9bf56286a4cf35ae5a
patriciarealini <hello@patriciarealini.com>
2015-09-15 01:09:14 -0700

    link button hover jquery to replace images

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 //main.js
+
+//hover effect for bottom link buttons
+$(document).ready(function() {
+	$('#twitter').mouseover(function() {
+		$(this.attr("class", "twitterafter"))
+	});
+	$('#twitter').mouseout(function() {
+		$(this.attr("class", "twitterbefore"))
+	});
+	$('#github').mouseover(function() {
+		$(this.attr("class", "githubafter"))
+	});
+	$('#github').mouseout(function() {
+		$(this.attr("class", "githubbefore"))
+	});
+	$('#linkedin').mouseover(function() {
+		$(this.attr("class", "linkedinafter"))
+	});
+	$('#linkedin').mouseout(function() {
+		$(this.attr("class", "linkedinbefore"))
+	});
+});
