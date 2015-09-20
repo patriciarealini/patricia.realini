bf46027e6e90481fd1ff3c2703f2b9d9a59b8b2c
patriciarealini <hello@patriciarealini.com>
2015-09-20 15:39:37 -0700

    commented everything out while i try to solve the JQuery/CSS animation i am trying to achieve

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 //main.js
 
-$(document).ready(function() {
-//hover effect that changes background color for links when hovering
-	$('.highlight').hoverHighlight('#E4D2E4'); 
+// $(document).ready(function() {
+// hover effect that changes background color for links when hovering
+//	$('.highlight').mouseover(function() {
+//		$(this.effects("highlight", {color: "#E3D0E3"}, 3000))
+//	}); 
 
-//hover effect for bottom link buttons
-	$('#twitter').mouseover(function() {
-		$(this.attr("class", "twitterafter"))
-	});
-	$('#twitter').mouseout(function() {
-		$(this.attr("class", "twitterbefore"))
-	});
-	$('#github').mouseover(function() {
-		$(this.attr("class", "githubafter"))
-	});
-	$('#github').mouseout(function() {
-		$(this.attr("class", "githubbefore"))
-	});
-	$('#linkedin').mouseover(function() {
-		$(this.attr("class", "linkedinafter"))
-	});
-	$('#linkedin').mouseout(function() {
-		$(this.attr("class", "linkedinbefore"))
-	});
+// // hover effect for bottom link buttons
+// 	$('#twitter').mouseover(function() {
+// 		$(this.attr("class", "twitterafter"))
+// 	});
+// 	$('#twitter').mouseout(function() {
+// 		$(this.attr("class", "twitterbefore"))
+// 	});
+// 	$('#github').mouseover(function() {
+// 		$(this.attr("class", "githubafter"))
+// 	});
+// 	$('#github').mouseout(function() {
+// 		$(this.attr("class", "githubbefore"))
+// 	});
+// 	$('#linkedin').mouseover(function() {
+// 		$(this.attr("class", "linkedinafter"))
+// 	});
+// 	$('#linkedin').mouseout(function() {
+// 		$(this.attr("class", "linkedinbefore"))
+// 	});
 });
