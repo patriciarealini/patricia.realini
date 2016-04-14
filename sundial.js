429b97221c32fecbeb9901176b035a9f38f7f0be
patriciarealini <hello@patriciarealini.com>
2016-04-14 01:21:47 -0700

    Comment out notes, use to create object

--- sundial.js
+++ sundial.js
@@ -8,76 +8,75 @@ var moment =
 
 document.getElementByClass("body").style.background-color = "red";
 
-//
-0000 Midnight
-  primary color
-  secondary color
-0100
-  primary color
-  secondary color
-0200
-  primary color
-  secondary color
-0300
-  primary color
-  secondary color
-0400
-  primary color
-  secondary color
-0500 Sunrise
-  primary color
-  secondary color
-0600 Dewy Morning Greens
-  primary color #CFCB9C
-  secondary color #908B45
-0700 Poolside Morning Aqua
-  primary color
-  secondary color
-0800 Skyline Morning Blue
-  primary color #B3DAE1
-  secondary color #469ABF
-0900 Lilac
-  primary color
-  secondary color
-1000 Warm Morning Rose
-  primary color #CE9486
-  secondary color #972D15
-1100 Lunch Rust
-  primary color
-  secondary color
-1200 High Noon Yellow
-  primary color #E8C95D
-  secondary color #C68006
-1300 Tomato
-  primary color
-  secondary color
-1400 Dark Afternoon Tan
-  primary color #AD6E45
-  secondary color #433022
-1500
-  primary color
-  secondary color
-1600
-  primary color
-  secondary color
-1700 Golden Hour
-  primary color
-  secondary color
-1800 Sundown
-  primary color
-  secondary color
-1900
-  primary color
-  secondary color
-2000
-  primary color
-  secondary color
-2100
-  primary color
-  secondary color
-2200
-  primary color
-  secondary color
-2300
-  primary color
-  secondary color
+// 0000 Midnight
+//   primary color
+//   secondary color
+// 0100
+//   primary color
+//   secondary color
+// 0200
+//   primary color
+//   secondary color
+// 0300
+//   primary color
+//   secondary color
+// 0400
+//   primary color
+//   secondary color
+// 0500 Sunrise
+//   primary color
+//   secondary color
+// 0600 Dewy Morning Greens
+//   primary color #CFCB9C
+//   secondary color #908B45
+// 0700 Poolside Morning Aqua
+//   primary color
+//   secondary color
+// 0800 Skyline Morning Blue
+//   primary color #B3DAE1
+//   secondary color #469ABF
+// 0900 Lilac
+//   primary color
+//   secondary color
+// 1000 Warm Morning Rose
+//   primary color #CE9486
+//   secondary color #972D15
+// 1100 Lunch Rust
+//   primary color
+//   secondary color
+// 1200 High Noon Yellow
+//   primary color #E8C95D
+//   secondary color #C68006
+// 1300 Tomato
+//   primary color
+//   secondary color
+// 1400 Dark Afternoon Tan
+//   primary color #AD6E45
+//   secondary color #433022
+// 1500
+//   primary color
+//   secondary color
+// 1600
+//   primary color
+//   secondary color
+// 1700 Golden Hour
+//   primary color
+//   secondary color
+// 1800 Sundown
+//   primary color
+//   secondary color
+// 1900
+//   primary color
+//   secondary color
+// 2000
+//   primary color
+//   secondary color
+// 2100
+//   primary color
+//   secondary color
+// 2200
+//   primary color
+//   secondary color
+// 2300
+//   primary color
+//   secondary color
